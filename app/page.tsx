9f9f00af1442de5325496d1270f41f4560a922db
Devin Lane <devin.c.lane@gmail.com>
2024-08-09 16:19:11 -0700

    add google oauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import SignIn from "@/components/sign-in";
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center p-24 gap-8">
             <h1>Guitar Student Portal</h1>
+            <SignIn />
         </main>
     );
 }
