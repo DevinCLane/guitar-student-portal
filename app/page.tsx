b70882b7baf4783795fbe1bc1621f0c3369cbdbc
Devin Lane <devin.c.lane@gmail.com>
2024-08-19 10:45:45 -0700

    change background color

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     let user = session?.user?.email;
 
     return (
-        <main className="flex min-h-screen flex-col items-center p-24 gap-8">
+        <main className="flex min-h-screen flex-col items-center p-24 gap-8 bg-gradient-to-r from-indigo-500">
             <h1>Guitar Student Portal</h1>
             <section>
                 {user ? <SignOut>{`Welcome ${user}`}</SignOut> : <SignIn />}
